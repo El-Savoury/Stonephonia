a895bdcbdabb012b1540a16f6418998170e93d21
El-Savoury <ross_morgan@outlook.com>
2022-12-08 01:11:32 +0000

    Added Colours class to hold custom colour values

---
+++ Stonephonia/Colours.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Stonephonia
+{
+    public static class Colours
+    {
+        public static Color lightBlue = new Color(177, 255, 242);
+        public static Color greyBlue = new Color(112, 181, 178);
+        public static Color greenBlue = new Color(81, 162, 165);
+        public static Color darkBlue = new Color(6, 101, 122);
+
+    }
+}
